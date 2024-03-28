da14f2316a2197d55dd81053fb98cd6d509efc77
marlinilram <majcjc@gmail.com>
2024-03-28 16:47:34 +0800

    ![Update] remove motion

--- hybrid.js
+++ hybrid.js
@@ -288,30 +288,30 @@ const vertexShaderSource = `
   void main () {
       gl_Position = vec4(0.0, 0.0, 2.0, 1.0);
 
-      uvec4 motion1 = texelFetch(u_texture, ivec2(((uint(index) & 0x3ffu) << 2) | 3u, uint(index) >> 10), 0);
-      vec2 trbf = unpackHalf2x16(motion1.w);
-      float dt = time - trbf.x;
+      // uvec4 motion1 = texelFetch(u_texture, ivec2(((uint(index) & 0x3ffu) << 2) | 3u, uint(index) >> 10), 0);
+      // vec2 trbf = unpackHalf2x16(motion1.w);
+      // float dt = time - trbf.x;
 
-      float topacity = exp(-1.0 * pow(dt / trbf.y, 2.0));
-      if(topacity < 0.02) return;
+      // float topacity = exp(-1.0 * pow(dt / trbf.y, 2.0));
+      // if(topacity < 0.02) return;
 
-      uvec4 motion0 = texelFetch(u_texture, ivec2(((uint(index) & 0x3ffu) << 2) | 2u, uint(index) >> 10), 0);
+      // uvec4 motion0 = texelFetch(u_texture, ivec2(((uint(index) & 0x3ffu) << 2) | 2u, uint(index) >> 10), 0);
       uvec4 static0 = texelFetch(u_texture, ivec2(((uint(index) & 0x3ffu) << 2), uint(index) >> 10), 0);
 
-      vec2 m0 = unpackHalf2x16(motion0.x), m1 = unpackHalf2x16(motion0.y), m2 = unpackHalf2x16(motion0.z), 
-           m3 = unpackHalf2x16(motion0.w), m4 = unpackHalf2x16(motion1.x); 
+      // vec2 m0 = unpackHalf2x16(motion0.x), m1 = unpackHalf2x16(motion0.y), m2 = unpackHalf2x16(motion0.z), 
+      //      m3 = unpackHalf2x16(motion0.w), m4 = unpackHalf2x16(motion1.x); 
       
-      vec4 trot = vec4(unpackHalf2x16(motion1.y).xy, unpackHalf2x16(motion1.z).xy) * dt;
-      vec3 tpos = (vec3(m0.xy, m1.x) * dt + vec3(m1.y, m2.xy) * dt*dt + vec3(m3.xy, m4.x) * dt*dt*dt);
+      // vec4 trot = vec4(unpackHalf2x16(motion1.y).xy, unpackHalf2x16(motion1.z).xy) * dt;
+      // vec3 tpos = (vec3(m0.xy, m1.x) * dt + vec3(m1.y, m2.xy) * dt*dt + vec3(m3.xy, m4.x) * dt*dt*dt);
       
-      vec4 cam = view * vec4(uintBitsToFloat(static0.xyz) + tpos, 1);
+      vec4 cam = view * vec4(uintBitsToFloat(static0.xyz), 1);
       vec4 pos = projection * cam;
   
       float clip = 1.2 * pos.w;
       if (pos.z < -clip || pos.x < -clip || pos.x > clip || pos.y < -clip || pos.y > clip) return;
       uvec4 static1 = texelFetch(u_texture, ivec2(((uint(index) & 0x3ffu) << 2) | 1u, uint(index) >> 10), 0);
 
-      vec4 rot = vec4(unpackHalf2x16(static0.w).xy, unpackHalf2x16(static1.x).xy) + trot;
+      vec4 rot = vec4(unpackHalf2x16(static0.w).xy, unpackHalf2x16(static1.x).xy);
       vec3 scale = vec3(unpackHalf2x16(static1.y).xy, unpackHalf2x16(static1.z).x);
       rot /= sqrt(dot(rot, rot));
   
@@ -335,14 +335,14 @@ const vertexShaderSource = `
       float radius = length(vec2((cov2d[0][0] - cov2d[1][1]) / 2.0, cov2d[0][1]));
       float lambda1 = mid + radius, lambda2 = mid - radius;
   
-      if(lambda2 < 0.0) return;
+      // if(lambda2 < 0.0) return;
       vec2 diagonalVector = normalize(vec2(cov2d[0][1], lambda1 - cov2d[0][0]));
       vec2 majorAxis = min(sqrt(2.0 * lambda1), 1024.0) * diagonalVector;
       vec2 minorAxis = min(sqrt(2.0 * lambda2), 1024.0) * vec2(diagonalVector.y, -diagonalVector.x);
       
       uint rgba = static1.w;
       vColor = 
-        clamp(pos.z/pos.w+1.0, 0.0, 1.0) * 
+        // clamp(pos.z/pos.w+1.0, 0.0, 1.0) * 
         vec4(1.0, 1.0, 1.0, topacity) *
         vec4(
           (rgba) & 0xffu, 
